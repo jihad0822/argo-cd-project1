7a465d8a4d89c2ef670ad2c0dd2a61bf3f504dbb
Jihad Muhammad <jihadmuhammad822@gmail.com>
2025-07-28 13:09:57 -0400

    Initial commit for argo-cd-project1

---
+++ terraform/1-providers.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = local.region
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12" # Use a recent version
+    }
+  }
+} 
